5e514ed50ca4cc668ed990181a4fefc440b88903
boyboy0710 <boyboy0710@naver.com>
2021-04-25 18:33:40 +0900

    test

--- src/main/java/open_python_File/Main.java
+++ src/main/java/open_python_File/Main.java
@@ -11,6 +11,7 @@ public class Main {
         interpreter.execfile("src/main/java/open_python_File/python_file/test.py");//파이썬 파일 불러오기 및 실행
         interpreter.execfile("src/main/java/open_python_File/python_file/test2.py");
         interpreter.exec("print('test')"); //괄호안에 있는 파이썬 구문 실행
+        
         /*
         파이썬 구문중 ""는 ''로 대체한다
         이 구문들은 자이썬 api가 있어야 한다
